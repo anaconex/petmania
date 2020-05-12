5a80a8da7a545c8dc34d2798b546c3c62a0087c0
Jawaid94 <jawaid94@outlook.com>
2020-05-12 16:16:08 +0500

    Petmania customization

---
+++ hcs_petmania_customization/models/models.py
@@ -0,0 +1,21 @@
+from odoo import models, fields, api
+
+
+class PurchaseOrderLine(models.Model):
+    _inherit = 'purchase.order.line'
+
+    weight = fields.Float(related='product_id.weight')
+    total_weight = fields.Float(compute='_compute_total_weight')
+
+    def _compute_total_weight(self):
+        for record in self:
+            record.total_weight = record.weight * record.product_qty
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    pet = fields.Selection([('cat_food', 'Cat Food'), ('dog_food', 'Dog Food'), ('cat & dog', 'Cat & Dog'),
+                            ('accessories', 'Accessories')])
+    food_type = fields.Selection([('dry', 'Dry'), ('wet', 'Wet')])
+    product_type = fields.Selection([('local', 'Local'), ('import', 'Import')])
